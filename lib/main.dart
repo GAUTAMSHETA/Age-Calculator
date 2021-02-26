8ee1db0e6f5c34aad5b3411c7fe6751372b036cf
GAUTAMSHETA <78837362+GAUTAMSHETA@users.noreply.github.com>
2021-02-27 00:15:01 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:age_calculator/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Age Calculator",
+      home: HomePage(),
+    );
+  }
+}
